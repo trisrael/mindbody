c19b083343fa4570e9be4d2b5f1d00423c72579e
Tristan Goffman <tgoffman@gmail.com>
2011-05-03 12:42:59 -0700

    getting syntax correct for handsoap

--- lib/classService.rb
+++ lib/classService.rb
@@ -1,31 +1,27 @@
+class MBClassService < MBService
+	endpoint :version => 1, :uri => self.get_api_namespace() + "/ClassService.asmx?wsdl" 
 
-
-
-class MBClassService extends MBAPIService
-
-	endpoint GetApiNamespace() + "/ClassService.asmx?wsdl"
 	
-	/**
-	 * Returns the raw result of the MINDBODY SOAP call.
-	 * @param pageSize
-	 * @param currentPage
-	 * @param xmlDetail
-	 * @param fields
-	 * @param credentials A source credentials object to use with this call
-	 * @return object The raw result of the SOAP call
-	 */
-	def getClasses(classDescriptionIDs, classIDs, staffIDs, startDate, endDate, clientID = null, pageSize = null, currentPage = null, xmlDetail = XMLDetail::Full, fields = null,  credentials = null)
+	 # Returns the raw result of the MINDBODY SOAP call.
+	 # @param pageSize
+	 #@param currentPage
+	 # @param xmlDetail
+	 # @param fields
+	 # @param credentials A source credentials object to use with this call
+	 # @return object The raw result of the SOAP call
+	
+	def getClasses(classDescriptionIDs, classIDs, staffIDs, startDate, endDate, clientID = nil, pageSize = nil, currentPage = nil, xmlDetail = XMLDetail::Full, fields = nil,  credentials = nil)
 
-		response = invoke_with_credentials( ns("GetClasses") ) do |msg|
-			msg.add('ClassDescriptionIDs'), classDescriptionIDs if classDescriptionIDs
-			msg.add('ClassIDs'), classIDs if classIDs
-			msg.add('StaffIDs'), staffIDs if staffIDs
-			msg.add('StartDateTime'), startDate if startDate
-			msg.add('endDateTime'), endDate if endDate
-			msg.add('ClientID'), clientID if clientID
+		response = invoke_with_credentials("GetClasses") do |msg|
+			msg.add 'ClassDescriptionIDs', classDescriptionIDs if classDescriptionIDs
+			msg.add 'ClassIDs', classIDs if classIDs
+			msg.add 'StaffIDs', staffIDs if staffIDs
+			msg.add 'StartDateTime', startDate if startDate
+			msg.add 'endDateTime', endDate if endDate
+			msg.add 'ClientID', clientID if clientID
 		end
 
 		return response
-	end		
+	end
 end
 
--- lib/mbAPI.rb
+++ lib/mbAPI.rb
@@ -12,18 +12,13 @@ module MindbodyAPI
 		"http://clients.mindbodyonline.com/api/0_5"
 	end
 
-	#Append namespace to provided string (when @xml_ns) instance variable available
-	def ns(string)
-		@xmls_ns + string
-	end
 
 end
 
 #MB Request Objects
 class SourceCredentials
-	include MindbodyAPI
 
-	attr_accessor :source_name, :password, :site_ids, :xml_ns
+	attr_accessor :source_name, :password, :site_ids
 	  
 	def initialize(source_name, password, site_ids)
 		 @source_name = source_name
@@ -34,32 +29,50 @@ class SourceCredentials
 	#Takes members of class and returns XML versions with each as sibling nodes to one another
 	#Example:
 	#	<ns:node1>text</ns:node1><ns:node2>text</ns:node2>
-	#
-	def to_soap
-				
+	def build_soap!(msg)
+		msg.add "SourceCredentials" do |node|
+			node.add "SourceName", @username
+			node.add "Password", @password
+			node.add "siteIDs" do |inner_node|
+				@site_ids.each do |i|
+					inner_node.add "int", i
+				end
+			end		
+		end
 	end
 
 end
 
 class UserCredentials
-	include MindbodyAPI
-
-	attr_accessor :username, :password, :site_ids, :xml_ns
+	attr_accessor :username, :password, :site_ids
 	  
 	def initialize(username, password, site_ids)
-		 @username = username
-		 @password = password
-		 @site_ids = site_ids
+		@username = username
+		@password = password	
+
+		begin
+			site_ids.send(:count) #Is this an array?
+		rescue
+			site_ids = site_ids = [site_ids]
+		end		
+				
+		@site_ids = site_ids
 	end
 	
 	#Takes members of class and returns XML versions with each as sibling nodes to one another
 	#Example:
 	#	<ns:node1>text</ns:node1><ns:node2>text</ns:node2>
-	#
-	def to_soap
-		
+	def build_soap!(msg)
+		msg.add "UserCredentials"	do |node|
+			node.add "Username", @username
+			node.add "Sassword", @password
+			node.add "SiteIDs" do |inner_node|
+				@site_ids.each do |i|
+					inner_node.add "int", i
+				end
+			end		
+		end
 	end
-
 end
 
 #Constants for xmldetail parameter of Mindbody SOAP calls
@@ -73,35 +86,31 @@ end
 #authentication purposes. The :invoke_with_credentials method includes the aforementioned members
 #in the SOAP XML to be transmitted to the Mindbody server
 class MBService < Handsoap::Service
-	include MindbodyAPI
+	extend MindbodyAPI
 
-	#Namespace for XML SOAP document 
-	attr_accessor :xms_ns
+	#Member variables 
+	attr_accessor :xms_ns, :sourceCredentials, :userCredentials
 
  	on_create_document do |doc|
     	doc.alias @xml_ns, GetApiNamespace()
 	end
 
-	attr_accessor :sourceCredentials, :userCredentials
-
 	#You can store a set of default SourceCredentials in the object if you are using the 
 	#same ones multiple times.
 	#@param SourceCredentials $credentials
-	def initialize(defaultCreds = null, defaultUserCreds = null)
+	def initialize(defaultCreds = null, defaultUserCreds = null, xml_ns)
 		@sourceCredentials = defaultCreds;
 		@userCredentials = defaultUserCreds;
-
-		@sourceCredentials.xml_ns = @xml_ns
-		@userCredentials.xml_ns = @xml_ns
+		@xml_ns = xml_ns
 	end
 	
 	#Invoke the @param service_name, name of SOAP service to be called
 	#including the sourceCredentials and userCredentials, then the method yields 	
 	def invoke_with_credentials(service_name)
 		response = invoke(service_name) do |msg|
-			msg.add ns('SourceCredentials'), sourceCredentials.to_soap if sourceCredentials
-			msg.add ns('UserCredentials'), userCredentials.to_soap if userCredentials
-			yield(msg)	#Allow block to add additional entries into SOAP request
+			sourceCredentials.build_soap!(msg) if sourceCredentials
+			userCredentials.build_soap!(msg) if userCredentials
+			yield(msg) #Allow block to add additional entries into SOAP request
 		end
 	end
 
