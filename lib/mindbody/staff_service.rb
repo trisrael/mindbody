7903a1d97f12a7d2e79823770f9738f980d13360
Tristan Goffman <tgoffman@gmail.com>
2011-06-21 10:30:45 -0700

    overwritten build_request method attempting to force need of options to be passed in, remove this reliance

--- lib/mindbody/staff_service.rb
+++ lib/mindbody/staff_service.rb
@@ -16,11 +16,5 @@ module Mb
 			@arr.each {|ent| throw TypeError.new "Expected entry to be of type #{StaffOptions.to_s}, but was of type #{ent.class.to_s}" if ent.class != StaffOptions}
 		end
 
-		def build_request(opts = {})
-			to_ret = super opts
-			@arr.each {|ent| to_ret.deep_merge! ent.to_hash}		
-			to_ret
-		end
-
 	end
 end
